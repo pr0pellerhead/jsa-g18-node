3b776ea94fb3500871bf84e89272ab5c96109bd6
Bojan Gavrovski <bojan@beyondbasics.co>
2022-07-21 20:58:16 +0200

    c13 webapp setup

---
+++ c13/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const pages = require('./handlers/pages');
+const callbacks = require('./handlers/callbacks');
+
+const dsn = '';
+
+mongoose.connect(
+    dsn,
+    err => {
+        if(err) return console.log(err);
+        console.log('Connected to DB');
+    }
+);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', pages.main); // all blogposts
+app.get('/blogpost/:id', pages.singleBlogpost); // single blogpost
+app.get('/form', pages.formAdd); // form to create new blogpost
+app.get('/form/:id', pages.formEdit); // form to edit a blogpost
+
+app.post('/callback/post', callbacks.create); // creates new blogpost
+app.post('/callback/post/:id', callbacks.edit); // edits existing blogpost
+app.post('/callback/remove/:id', callbacks.remove); // deletes a blogpost
+
+app.listen(10000, err => {
+    if(err) return console.log(err);
+    console.log('Server successfully started on port 10000');
+});
